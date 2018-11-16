f31c645a4306a8aadf6ac28366fa3dcfdb628c38
bacha <bachatero_dany@outlook.es>
2018-11-15 23:15:12 -0600

    se creo la actividad detalleMascota

---
+++ app/src/main/java/com/example/bacha/puppy/DetalleMascota.java
@@ -0,0 +1,14 @@
+package com.example.bacha.puppy;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetalleMascota extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.mascota_detalle);
+    }
+}
